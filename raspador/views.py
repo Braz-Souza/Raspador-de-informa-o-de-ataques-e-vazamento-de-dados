9807f998e4816ee39ac2448521cd096499fada5f
BILlS27 <gabrielsodre59@gmail.com>
2022-12-11 11:12:24 -0300

    Botão que faz a raspagem

--- raspador/views.py
+++ raspador/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-
 from .models import Noticia
+from .raspador import pesquisar_por_palavra_chaves_e_depois_salvar_noticias;
+
 
 def index(request):
+    
     noticias = Noticia.objects.order_by('id')
     template = loader.get_template('raspador/noticias.html')
     context = {
         'noticias': noticias,
     }
+    if (request.GET.get('btnraspador')):
+        pesquisar_por_palavra_chaves_e_depois_salvar_noticias(Noticia);
     return HttpResponse(template.render(context, request))
